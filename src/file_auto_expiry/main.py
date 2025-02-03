adc6df2e8c2fe15ea09f343cec6abe3188fbdcb9
cehune <mckale.chung@gmail.com>
2025-02-03 23:56:22 +0000

    Add flag to overwrite or append new info to an existing file

--- src/file_auto_expiry/main.py
+++ src/file_auto_expiry/main.py
@@ -5,7 +5,7 @@ import typer
 app = typer.Typer()
 
 @app.command()
-def collect_file_info(path: str, save_file: str = "", days_for_expiry: int = 10, check_folder_atime: bool = False):
+def collect_file_info(path: str, save_file: str = "", days_for_expiry: int = 10, check_folder_atime: bool = False, overwrite_file: bool = True):
     """
     Collects information about the top level paths within a given folder path
     And dumps it into a json file, specified by the save_file flag
@@ -17,19 +17,22 @@ def collect_file_info(path: str, save_file: str = "", days_for_expiry: int = 10,
                                      save_file=save_file, 
                                      scrape_time=scrape_time, 
                                      expiry_threshold=expiry_threshold,
-                                     check_folder_atime=check_folder_atime)
+                                     check_folder_atime=check_folder_atime,
+                                     overwrite_file=overwrite_file)
 
 @app.command()
-def collect_creator_info(file_info: str, save_file: str = ""):
+def collect_creator_info(file_info: str, save_file: str = "", overwrite_file: bool = True):
     """
     Tabulates the paths that relate to specific users, based on a given jsonl path
     That jsonl path should be the result of calling the collect_file_info function
     It then dumps the new information into another json file, specified by the save_file flag
     """
     scrape_time = time.time()
+    print(overwrite_file)
     collect_creator_information(path_info_file=file_info, 
                                 save_file=save_file, 
-                                scrape_time=scrape_time)
+                                scrape_time=scrape_time,
+                                overwrite_file=overwrite_file)
 
 if __name__ == "__main__":
     app()
